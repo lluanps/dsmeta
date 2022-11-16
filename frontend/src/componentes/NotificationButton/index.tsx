bf969676fdd8f886240edca2162f823c08e443a7
Luan Pinheiro <lluanps@gmail.com>
2022-11-16 15:23:12 -0300

    first component

---
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from 'C:/projetos/dsmeta/frontend/src/assets/img/Vector.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return(
+      <div className="vendas-botao">
+      <img src={icon} alt="notificar" />
+  </div>
+    )
+  }
+  
+  export default NotificationButton
+  
